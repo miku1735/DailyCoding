1429cb9b5bf49e06e93da5ee5f0797690869cea1
Mayank <Mayank13@ibm.com>
2021-06-28 12:19:44 +0530

    Create Day55_Q110_Array_ReverseTheArray.java

---
+++ Array/Day55_Q110_Array_ReverseTheArray.java
@@ -0,0 +1,37 @@
+
+
+
+// Reverse the array to
+
+/**
+ * You are given a string s. You need to reverse the string.
+
+Example 1:
+
+Input:
+s = Geeks
+Output: skeeG
+Example 2:
+
+Input:
+s = for
+Output: rof
+Your Task:
+
+You only need to complete the function reverseWord() that takes s as parameter and returns the reversed string.
+
+Expected Time Complexity: O(|S|).
+Expected Auxiliary Space: O(1).
+
+Constraints:
+1 <= |s| <= 10000
+ */
+
+class Reverse
+{
+    
+    public static String reverseWord(String str)
+    {
+        return new StringBuilder(str).reverse().toString();
+    }
+}
