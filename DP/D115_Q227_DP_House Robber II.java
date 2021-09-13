bc865f570d2e525942b04b2b024798a2c79dd1cd
Mayank <Mayank13@ibm.com>
2021-09-13 23:51:34 +0530

    Create D115_Q227_DP_House Robber II.java

---
+++ DP/D115_Q227_DP_House Robber II.java
@@ -0,0 +1,61 @@
+
+
+
+// House Robber II
+
+
+/**
+ * You are a professional robber planning to rob houses along a street. Each house has a certain amount of money stashed. All houses at this place are arranged in a circle. That means the first house is the neighbor of the last one. Meanwhile, adjacent houses have a security system connected, and it will automatically contact the police if two adjacent houses were broken into on the same night.
+
+Given an integer array nums representing the amount of money of each house, return the maximum amount of money you can rob tonight without alerting the police.
+
+ 
+
+Example 1:
+
+Input: nums = [2,3,2]
+Output: 3
+Explanation: You cannot rob house 1 (money = 2) and then rob house 3 (money = 2), because they are adjacent houses.
+Example 2:
+
+Input: nums = [1,2,3,1]
+Output: 4
+Explanation: Rob house 1 (money = 1) and then rob house 3 (money = 3).
+Total amount you can rob = 1 + 3 = 4.
+Example 3:
+
+Input: nums = [1,2,3]
+Output: 3
+ 
+
+Constraints:
+
+1 <= nums.length <= 100
+0 <= nums[i] <= 1000
+ */
+
+class Solution {
+    public int rob(int[] nums) {
+        if(nums.length==1) return nums[0];
+        return Math.max(find(Arrays.copyOfRange(nums,0,nums.length-1)), find(Arrays.copyOfRange(nums,1,nums.length)));
+    }
+    public int find(int[] nums) {
+        int size = nums.length;
+        int[] dump = new int[size];
+        if(size == 1) return nums[0];
+        if(size == 2) return Math.max(nums[0],nums[1]); 
+        
+        dump[0] = nums[0];
+        dump[1] = Math.max(nums[0],nums[1]);;
+        
+        for(int i = 2; i<size; i++){
+            if(nums[i]+dump[i-2]>dump[i-1]){
+                dump[i] = nums[i]+dump[i-2];
+            }
+            else{
+                dump[i] = dump[i-1];
+            }
+        }
+        return dump[size-1];
+    }
+}
